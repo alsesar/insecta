dbe46107d2c7ca58e3e8949cf614734619966f68
Dz <adrielveig@gmail.com>
2026-05-19 14:07:02 -0300

    insecta login e cadastro

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,143 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { LinearGradient } from "expo-linear-gradient"
+import { Link } from "expo-router"
+import { useState } from "react"
+import { Alert, Image, StyleSheet, Text, View } from "react-native"
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+import { Button } from "@/components/button"
+import { Input } from "@/components/input"
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
 
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+export default function IndexPage() {
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState(" ")
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+   const entrar = () => {
 
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
-  );
+    if( !email.trim() || !password.trim()) {
+        return Alert.alert ("Entrar","Preencha email e senha para entrar")
+  
+          }
+            const regex = /@gmail\.com$/
+
+    if (!regex.test(email)) {
+        alert("Use um Gmail válido")
+        return
+    }
+
+   }
+
+    function handleSignIn() {
+        if( !email.trim() || !password.trim()) {
+      return Alert.alert ("Entrar","preencha email e senha para entrar")
+
+        }
+        //Alert.alert ("Entrar,Função de entrar acionada")
+            //console.log(email, password)
+    }
+
+    return(
+
+<LinearGradient colors={['#000166', '#0002CC']} style={styles.container}>
+     <View>
+            <Image source={require("../assets/logonaope.png")} style={styles.image}/>
+            <Text style={styles.title}>Olá!</Text>
+            <Text style={styles.subtitle}>Bem vindo ao insecta! Seu novo app para aprender</Text>
+
+                <View style={styles.form}>
+                    <Text style={styles.suptext}>Email:</Text>
+                    <Input 
+                        placeholder=" " 
+                        keyboardType="email-address" 
+                        onChangeText={setEmail}
+                        autoCapitalize="none"
+                    />
+
+                    <Text style={styles.suptext}>Senha:</Text>
+                     <Input 
+                        placeholder=" " 
+                        secureTextEntry
+                        onChangeText={setPassword}
+                    />
+                    <Text style={styles.footerText}> 
+                        Esquceu a senha? Clique aqui!
+                    </Text>
+                    <Button 
+                        label="Entrar"
+                        onPress={entrar}
+                    />
+                </View>
+
+            <Text style={styles.footerText}>
+
+            Não tem uma conta? {" "}
+            <Link href="/signup" style={styles.footerLink}>Crie aqui!</Link>
+
+            </Text>
+
+    </View>
+</LinearGradient>
+    
+   
+
+)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
+
+    container: {
+        flex: 1,
+        justifyContent:"center",
+        alignItems:"center",
+        padding: 20
+
+
+    },
+
+    image: {
+        width: 100,
+        height: 100,
+        position:"absolute",
+        top: 65,
+        marginLeft: 250
+    },
+
+    title: {
+        color:'#fff',
+        fontSize: 24,
+        fontWeight: "bold" 
+        
+    },
+
+    subtitle:{
+        color:'#fff',
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+
+    suptext:{
+        color:'#fff',
+
+    },
+    
+    form:{
+        marginTop:24,
+        gap: 12,
+       
+    },
+
+    footerText:{
+        color:"#fff",
+        marginLeft:5
+
+    },
+
+    footerLink:{
+        color:"#FFC43F",
+        fontWeight: 700
+     
+    }
+
+
+})
+
---
+++ src/app/signup.tsx
@@ -0,0 +1,164 @@
+import { LinearGradient } from "expo-linear-gradient"
+import { Link, router } from "expo-router"
+import { useState } from "react"
+import { Alert, Image, StyleSheet, Text, View } from "react-native"
+
+import { Button } from "@/components/button"
+import { Input } from "@/components/input"
+
+export default function IndexPage() {
+     const [email, setEmail] = useState("")
+       const [password, setPassword] = useState(" ")
+        const [confirmarsenha, setConfirmarsenha] = useState("")
+    
+    
+       const cadastrar = () => {
+    
+        if( !email.trim() || !password.trim()) {
+            return Alert.alert ("Entrar","Preencha todo os campos para se cadastrar")
+      
+              }
+                const regex = /@gmail\.com$/
+    
+        if (!regex.test(email)) {
+            alert("Use um Gmail válido")
+            return
+        }
+            const regexSenha =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/
+
+            if (!regexSenha.test(password)) {
+                alert("A senha precisa ter 8 caracteres, letras maiúsculas e minúsculas, números e caracteres especiais")
+            
+            return
+        }
+
+            if (password !== confirmarsenha) {
+                alert("As senhas não coincidem")
+                return
+         }
+            alert("Cadastro realizado com sucesso")
+            router.replace("/")
+
+             
+
+         }
+
+
+    return(
+
+<LinearGradient colors={['#000166', '#0002CC']} style={styles.container}>
+        <View>
+            <Image source={require("../assets/logo voando.png")} style={styles.image}/>
+            <Text style={styles.title}>Cadastre-se!</Text>
+            <Text style={styles.subtitle}>Crie uma conta para começar a aprender!</Text>
+
+            <View style={styles.form}>
+                <Text style={styles.suptext}>Nome:</Text>
+                <Input 
+                    placeholder=" "
+                />
+
+                <Text style={styles.suptext}>Email:</Text>
+                <Input 
+                    placeholder=" " 
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    onChangeText={setEmail}
+                />
+
+                <Text style={styles.suptext}>Senha:</Text>
+                <Input 
+                    placeholder=" "    
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+
+                />
+
+                <Text style={styles.suptext}>Confrimar senha:</Text>
+                <Input 
+                    placeholder=" " 
+                    secureTextEntry
+                    value={confirmarsenha}
+                    onChangeText={setConfirmarsenha}
+                />
+
+                <Button    
+                    label="Cadastrar"
+                    onPress={cadastrar}
+                />
+            </View>
+
+        <Text style={styles.footerText}>
+
+            Já tem uma conta?{" "}
+            <Link href="/" style={styles.footerLink}>Clique aqui!</Link>
+
+        </Text>
+
+    </View>
+</LinearGradient>
+    
+   
+)
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent:"center",
+        alignItems:"center",
+
+
+    },
+
+    image: {
+        width: 100,
+        height: 100,
+        position:"absolute",
+        top: 45,
+        marginLeft: 250
+    },
+
+    title: {
+        color:'#fff',
+        fontSize: 24,
+        fontWeight: "bold" 
+        
+    },
+
+    subtitle:{
+        color:'#fff',
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+
+    suptext:{
+        color:'#fff',
+
+    },
+    
+    form:{
+        marginTop:24,
+        gap: 12,
+       
+    },
+
+    footerText:{
+        textAlign:"center",
+        color:"#fff"
+
+    },
+
+    footerLink:{
+        textAlign:"center",
+        color:"#FFC43F",
+        fontWeight: 700
+     
+    }
+
+
+})
+
